4526ecadb414ecf27ab03c62f6294eb2eda15e46
Goncharoff <nik.ignition.work@gmail.com>
2023-03-14 17:10:38 +0200

    init

---
+++ src/main/java/com/example/testteststetetetet/TestTEststetetetetApplication.java
@@ -0,0 +1,13 @@
+package com.example.testteststetetetet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTEststetetetetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTEststetetetetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testteststetetetet/TestTEststetetetetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testteststetetetet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTEststetetetetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
